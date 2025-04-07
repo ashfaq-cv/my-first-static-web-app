069dede657337632ac7e5f3a5a73a26f05177363
Mohammed Ashfaq <ashfaqm@conceptvines.com>
2025-04-07 12:42:20 +0530

    Updated button text

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import React from 'react';
 function App() {
 <<<<<<< HEAD
   const value = 'My First React App deployement';
-  console.log('AAAA!');
+  console.log('AAAA');
   return <div>1234!! {value}</div>;
 =======
   const value = 'BBBB';
-  return <div>5678! {value}</div>;
+  return <div>5678 {value}</div>;
 >>>>>>> 6dea59dd03bac9270ec794e5d5811c2471b2ec7d
 }
 
