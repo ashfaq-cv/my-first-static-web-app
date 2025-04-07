274fbf854b64ac22e42a43082b1dc00f9890ec7f
Mohammed Ashfaq <ashfaqm@conceptvines.com>
2025-04-07 12:50:23 +0530

    Updated all clear  text

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
 
 function App() {
-<<<<<<< HEAD
-  const value = 'My First React App deployement';
+  const value = 'all clear';
   console.log('AAAA');
-  return <div>1234!! {value}</div>;
-=======
-  const value = 'BBBB';
-  return <div>5678 {value}</div>;
->>>>>>> 6dea59dd03bac9270ec794e5d5811c2471b2ec7d
+  return <div>Yes!! {value}</div>;
 }
 
 export default App;
