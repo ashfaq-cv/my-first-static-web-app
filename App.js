f20cbc2df017ad49de9c3baeceff424cba4f7ece
Mohammed Ashfaq <ashfaqm@conceptvines.com>
2025-04-07 12:09:38 +0530

    2nd commit

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function App() {
-  const value = 'My First React App';
+  const value = 'My First React App deployement';
   console.log('Hello World!');
   return <div>Welcome!! {value}</div>;
 }
