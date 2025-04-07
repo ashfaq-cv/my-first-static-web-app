35a78b617d37c71ed5d4481863edd282293577f8
Mohammed Ashfaq <ashfaqm@conceptvines.com>
2025-04-07 11:36:52 +0530

    commiting my first deployement

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function App() {
+  const value = 'My First React App';
+  console.log('Hello World!');
+  return <div>Welcome!! {value}</div>;
+}
+
+export default App;
